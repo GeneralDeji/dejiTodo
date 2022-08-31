8736850886c106be0d4498b375ded23ca42ccd47
GeneralDeji <otegbadeemmanuel@yahoo.com>
2022-08-31 15:46:55 +0100

    new

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Router = require('./routes/routesToDo')
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 7000
+const port = process.env.PORT || 7000
 const mongoose = require('mongoose')
 
 app.use(express.json());
